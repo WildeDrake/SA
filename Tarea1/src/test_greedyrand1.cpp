78cab15d4c0e330e240c79f81f3fd642e46197ec
WildeDrake <diegovenegasanabalon@gmail.com>
2025-09-22 19:39:56 -0300

    Ahora GreedyRand recibe k

--- Tarea1/src/test_greedyrand1.cpp
+++ Tarea1/src/test_greedyrand1.cpp
@@ -11,17 +11,24 @@ using namespace std;
 int main(int argc, char* argv[]) {
     string root = "../dataset_grafos_no_dirigidos";
 
-     if (argc < 3) {
-        cerr << "Uso: " << argv[0] << " -i <instancia-problema>\n";
+    if (argc < 3) {
+        cerr << "Uso: " << argv[0] << " -i <instancia-problema> [-k <valor-k>]\n";
         return 1;
     }
+
     string instancia;
+    int k = 10; // valor por defecto
     for (int i = 1; i < argc; i++) {
         string arg = argv[i];
         if (arg == "-i" && i + 1 < argc) {
             instancia = argv[i + 1];
+            i++;
+        } else if (arg == "-k" && i + 1 < argc) {
+            k = stoi(argv[i + 1]); // convierte string a int
+            i++;
         }
     }
+
     if (instancia.empty()) {
         cerr << "Error: no se especificó instancia con -i\n";
         return 1;
@@ -29,7 +36,7 @@ int main(int argc, char* argv[]) {
 
     string filename = root + "/" + instancia;
     Grafo grafo = parsearGrafo(filename);
-    int k = 10; // Tamaño de la lista de candidatos
+
     auto start = chrono::high_resolution_clock::now();
     vector<int> resultado = greedyRandK(grafo, k);
     auto end = chrono::high_resolution_clock::now();
@@ -39,7 +46,7 @@ int main(int argc, char* argv[]) {
         cerr << "Error: solución inválida\n";
         return 1;
     }
-    cout << resultado.size() << " " << elapsed.count() << "\n";
 
+    cout << resultado.size() << " " << elapsed.count() << "\n";
     return 0;
 }

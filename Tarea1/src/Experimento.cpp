045e687ca2a3f0cb1cb35a40e7734ef20e144a83
WildeDrake <diegovenegasanabalon@gmail.com>
2025-09-22 23:10:53 -0300

    Arrglado Experimento

--- Tarea1/src/Experimento.cpp
+++ Tarea1/src/Experimento.cpp
@@ -19,7 +19,7 @@ int main() {
         << "MediaGreedyRand1,PromTiempoGreedyRand1,"
         << "MediaGreedy2,PromTiempoGreedy2,"
         << "MediaGreedy3,PromTiempoGreedy3\n";
-
+    int reps = 5;
     for (int n : {1000, 2000, 3000}) {
         for (double dens = 0.1; dens <= 0.9; dens += 0.1) {
             double sum_g1 = 0, sum_t1 = 0;
@@ -37,63 +37,65 @@ int main() {
                 // -------- Greedy1 --------
                 double t_g1 = 0;
                 int res_g1 = 0;
-                for (int rep = 0; rep < 30; rep++) {
+                for (int rep = 0; rep < reps; rep++) {
                     Grafo g = parsearGrafo(file);
                     auto start = high_resolution_clock::now();
-                    res_g1 = greedy1(g).size();
+                    res_g1 += greedy1(g).size();
                     auto end = high_resolution_clock::now();
                     t_g1 += duration<double, milli>(end - start).count();
                 }
-                sum_g1 += res_g1 / 30.0;
-                sum_t1 += t_g1 / 30.0;
+                sum_g1 += res_g1 / reps;
+                sum_t1 += t_g1 / reps;
 
                 // -------- GreedyRand1 --------
                 double t_gr = 0;
                 int res_gr = 0;
-                for (int rep = 0; rep < 30; rep++) {
+                for (int rep = 0; rep < reps; rep++) {
                     Grafo g = parsearGrafo(file);
                     auto start = high_resolution_clock::now();
-                    res_gr = greedyRandK(g, n/100).size(); // K = n/100
+                    res_gr += greedyRandK(g, n/100).size(); // K = n/100
                     auto end = high_resolution_clock::now();
                     t_gr += duration<double, milli>(end - start).count();
                 }
-                sum_gr += res_gr / 30.0;
-                sum_tr += t_gr / 30.0;
+                sum_gr += res_gr / reps;
+                sum_tr += t_gr / reps;
 
                 // -------- Greedy2 --------
                 double t_g2 = 0;
                 int res_g2 = 0;
-                for (int rep = 0; rep < 30; rep++) {
+                for (int rep = 0; rep < reps; rep++) {
                     Grafo g = parsearGrafo(file);
                     auto start = high_resolution_clock::now();
-                    res_g2 = greedy21(g).size();
+                    res_g2 += greedy21(g).size();
                     auto end = high_resolution_clock::now();
                     t_g2 += duration<double, milli>(end - start).count();
                 }
-                sum_g2 += res_g2 / 30.0;
-                sum_t2 += t_g2 / 30.0;
+                sum_g2 += res_g2 / reps;
+                sum_t2 += t_g2 / reps;
 
                 // -------- Greedy3 --------
                 double t_g3 = 0;
                 int res_g3 = 0;
-                for (int rep = 0; rep < 30; rep++) {
+                for (int rep = 0; rep < reps; rep++) {
                     Grafo g = parsearGrafo(file);
                     auto start = high_resolution_clock::now();
-                    res_g3 = greedy22(g).size();
+                    res_g3 += greedy22(g).size();
                     auto end = high_resolution_clock::now();
                     t_g3 += duration<double, milli>(end - start).count();
                 }
-                sum_g3 += res_g3 / 30.0;
-                sum_t3 += t_g3 / 30.0;
+                sum_g3 += res_g3 / reps;
+                sum_t3 += t_g3 / reps;
             }
 
             // -------- Escribir promedios en CSV --------
-            out << n << "," << dens << ","
+            out << n << "," << fixed << setprecision(1) << dens << ","
                 << fixed << setprecision(2)
                 << sum_g1 / 30.0 << "," << sum_t1 / 30.0 << ","
                 << sum_gr / 30.0 << "," << sum_tr / 30.0 << ","
                 << sum_g2 / 30.0 << "," << sum_t2 / 30.0 << ","
                 << sum_g3 / 30.0 << "," << sum_t3 / 30.0 << "\n";
+
+            out.flush();
         }
     }
 

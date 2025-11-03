3e8841821307bcec7fd81ed855af4a25d2b050b6
Francy <114519802+parallals@users.noreply.github.com>
2025-11-03 02:12:29 -0300

    Genetic Algorithm + Tarea 3 creada

---
+++ Tarea3/utils.hpp
@@ -0,0 +1,19 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+struct Grafo {
+    int n;
+    vector<vector<int>> vecinos;
+    vector<int> grado;
+};
+
+Grafo parsearGrafo(const string& filename);
+bool validador(Grafo& grafo, vector<int>& conjunto);
+
+#endif 

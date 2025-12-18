8c3b621dda2044d53a5475756d502f562f62275d
WildeDrake <diegovenegasanabalon@gmail.com>
2025-12-18 00:50:01 -0300

    Reinicio Parcial Agregado

--- Tarea4/test_ACO.cpp
+++ Tarea4/test_ACO.cpp
@@ -20,8 +20,8 @@ int main(int argc, char* argv[]) {
     }
     // Leer parametros obligatorios 
     if (!args.count("-i")) {
-        cerr << "Uso: " << argv[0] << " -i <instancia> [-n <int>] [-a <double>] [-b <double>] [-e <double>] [-min <double>] [-max <double>]  [-r <int>] [-t <seg>] [-print <int>] [-irace <0|1>]\n";
-        cerr << "Ejemplo: ./test_ACO -i erdos_n1000_p0c0.1_1.graph -n 80 -a 1.0 -b 2.0 -e 0.1 -min 0.01 -max 6.0 -t 10 -irace 0\n";
+        cerr << "Uso: " << argv[0] << " -i <instancia> [-n <int>] [-a <double>] [-b <double>] [-e <double>] [-min <double>] [-max <double>]  [-r <int>] [-l <double>] [-t <seg>] [-print <int>] [-irace <0|1>]\n";
+        cerr << "Ejemplo: ./test_ACO -i erdos_n1000_p0c0.1_1.graph -n 80 -a 1.0 -b 2.0 -e 0.1 -min 0.01 -max 6.0 -r 100 -l 0.5t 10 -irace 0\n";
         return 1;
     }
     string filename = args["-i"];
@@ -33,12 +33,13 @@ int main(int argc, char* argv[]) {
     double tauMin = args.count("-min") ? stod(args["-min"]) : 0.01;
     double tauMax = args.count("-max") ? stod(args["-max"]) : 6.0;
     int resetThreshold = args.count("-r") ? stoi(args["-r"]) : 100;
+    double lambda = args.count("-l") ? stod(args["-l"]) : 0.5;
     int tiempoMaxSeg = args.count("-t") ? stoi(args["-t"]) : 10;
     bool print = args.count("-print") ? stoi(args["-print"]) : 0;
     bool irace = args.count("-irace") ? stoi(args["-irace"]) : 0;
 
     // Ejecutar ACO 
-    pair<double, vector<int>> resultado = ACO(filename, nHormigas, alpha, beta, evaporacion, tauMin, tauMax, resetThreshold, tiempoMaxSeg, print);
+    pair<double, vector<int>> resultado = ACO(filename, nHormigas, alpha, beta, evaporacion, tauMin, tauMax, resetThreshold, lambda, tiempoMaxSeg, print);
 
     // Validar solucion 
     Grafo g = parsearGrafo(filename);
